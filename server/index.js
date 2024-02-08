f3d096ac103b4ecc9d915d7925040a9fa8977c35
yanwd <yannickdjoa2@gmail.com>
2024-02-08 16:35:57 +0100

    connecting mongodb database to the project

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
-import express from "express"
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const app= express()
+const mongo = process.env.MONGO;
+const app = express();
+mongoose
+  .connect(mongo)
+  .then(() => {
+    console.log('connected to mongoDB');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-const port= process.env.PORT;
-app.listen(port, ()=>{
-
-    console.log(`app is up and listening to port ${port}`)
-})
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`app is up and listening to port ${port}`);
+});
