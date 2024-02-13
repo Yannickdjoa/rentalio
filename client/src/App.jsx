50c431c2aa0d9f331f5d6d452c1c7836c56e6847
yanwd <yannickdjoa2@gmail.com>
2024-02-13 16:25:09 +0100

    updated the Header and finalised the profile

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import React from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Profile from "./pages/Profile"
-import SignIn from "./pages/SignIn"
-import Home from "./pages/Home"
-import SignUp from "./pages/SignUp"
-import About from "./pages/About"
-import Header from "./components/Header"
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import Home from './pages/Home'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Header from './components/Header'
+import PrivateRoute from './components/PrivateRoute'
 
 function App() {
     return (
@@ -13,7 +14,10 @@ function App() {
             <Header />
             <Routes>
                 <Route exact path="/" Component={Home} />
-                <Route exact path="/profile" Component={Profile} />
+                <Route Component={PrivateRoute}>
+                    <Route exact path="/profile" Component={Profile} />
+                </Route>
+
                 <Route exact path="/sign-in" Component={SignIn} />
                 <Route exact path="/sign-up" Component={SignUp} />
                 <Route exact path="/about" Component={About} />
