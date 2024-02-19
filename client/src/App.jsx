6355433b32cf81ec56dda23cfc7cb0b8682a98e2
yanwd <yannickdjoa2@gmail.com>
2024-02-19 12:43:05 +0100

    completed image uploading fonctionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import SignIn from './pages/SignIn'
 import Home from './pages/Home'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
+import CreateListing from './pages/CreateListing'
 import Header from './components/Header'
 import PrivateRoute from './components/PrivateRoute'
 
@@ -16,11 +17,17 @@ function App() {
                 <Route exact path="/" Component={Home} />
                 <Route Component={PrivateRoute}>
                     <Route exact path="/profile" Component={Profile} />
+                    <Route
+                        exact
+                        path="/create-listing"
+                        Component={CreateListing}
+                    />
                 </Route>
 
                 <Route exact path="/sign-in" Component={SignIn} />
                 <Route exact path="/sign-up" Component={SignUp} />
                 <Route exact path="/about" Component={About} />
+                <Route exact path="/create-listing" Component={CreateListing} />
             </Routes>
         </BrowserRouter>
     )
