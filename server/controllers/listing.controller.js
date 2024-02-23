11d63dca69985b57f441e0ad34a2412bb9c5f10b
yanwd <yannickdjoa2@gmail.com>
2024-02-23 11:11:19 +0100

    search api set

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -59,3 +59,56 @@ export const getListing = async (req, res, next) => {
     next(error);
   }
 };
+export const getListings = async (req, res, next) => {
+  try {
+    const limit = parseInt(req.query.limit) || 9;
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    let searchTerm = req.query.searchTerm || '';
+    let type = req.query.type;
+    if (type === undefined || type === 'all') {
+      type = { $in: ['sell', 'rent'] };
+    }
+    let offer = req.query.offer;
+    if (offer === undefined || offer === 'false') {
+      offer = { $in: [true, false] };
+    }
+
+    let parking = req.query.parking;
+    if (parking === undefined || parking === 'false') {
+      parking = { $in: [true, false] };
+    }
+    let furnished = req.query.furnished;
+    if (furnished === undefined || furnished === 'false') {
+      furnished = { $in: [true, false] };
+    }
+    let sort = req.query.sort || 'createdAt';
+    let order = req.query.order || 'desc';
+
+    const listings = await Listing.find({
+      $or: [
+        {
+          name: {
+            $regex: searchTerm,
+            $options: 'i',
+          },
+        },
+        {
+          description: {
+            $regex: searchTerm,
+            $options: 'i',
+          },
+        },
+      ],
+      type,
+      offer,
+      parking,
+      furnished,
+    })
+      .sort({ [sort]: order })
+      .skip(startIndex)
+      .limit(limit);
+    return res.status(200).json(listings);
+  } catch (error) {
+    next(error);
+  }
+};
